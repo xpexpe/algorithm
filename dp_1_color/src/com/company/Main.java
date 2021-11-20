bc1db5251926b378241ca475a84afdd1c54b20fc
xpexpe <84072084+xpexpe@users.noreply.github.com>
2021-11-20 23:56:56 +0900

    # CHECKPOINT 1

--- dp_1_color/src/com/company/Main.java
+++ dp_1_color/src/com/company/Main.java
@@ -1,8 +1,43 @@
 package com.company;
 
+/*
+r=3 | 1 0 0
+r=2 | 0 1 1
+r=1 | 1 0 0
+     _______
+  c=  1 2 3
+
+  solve
+
+ */
 public class Main {
+    static int col[][] = new int[5][5];
+
+    static void init() {
+        for(var i : col)
+            for(var j : i)
+                j=0;
+    }
+
+    static void set() {
+
+    }
+
+    static void change(int r, int c) {
+        if(col[r][c] == 0)  col[r][c] = 1;
+        col[r][c] = 0;
+    }
+
+    static void reverse(int r, int c) {
+        change(r, c);
+        change(r-1, c);
+        change(r, c-1);
+        change(r+1, c);
+        change(r, c+1);
+    }
 
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
+
