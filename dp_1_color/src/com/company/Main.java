8076c103e3e35ff81eae293cea5db7a1db416491
xpexpe <xpexpe2@chosun.kr>
2021-11-21 04:03:23 +0900

    # TEST 1

--- dp_1_color/src/com/company/Main.java
+++ dp_1_color/src/com/company/Main.java
@@ -1,5 +1,10 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 /*
 r=3 | 1 0 0
 r=2 | 0 1 1
@@ -7,25 +12,22 @@ r=1 | 1 0 0
      _______
   c=  1 2 3
 
-  solve
+  solve : 5x5 -> 1~3 index search
 
  */
 public class Main {
     static int col[][] = new int[5][5];
-
+    static int check = 1;
+    static int end = 0;
     static void init() {
         for(var i : col)
             for(var j : i)
                 j=0;
     }
 
-    static void set() {
-
-    }
-
     static void change(int r, int c) {
         if(col[r][c] == 0)  col[r][c] = 1;
-        col[r][c] = 0;
+        else col[r][c] = 0;
     }
 
     static void reverse(int r, int c) {
@@ -34,10 +36,35 @@ public class Main {
         change(r, c-1);
         change(r+1, c);
         change(r, c+1);
+        int sum = 0;
+        for(int i=1; i<4; i++) {
+            for(int j=1; j<4; j++) {
+                sum += col[i][j];
+            }
+        }
+        if(sum == 0 || sum == 9) end = check;
+        check++;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        init();
+        for(int i=1; i<4; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j=1; j<4; j++) {
+                col[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
 
+        int N = Integer.parseInt(br.readLine());
+        for(int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            reverse(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+        }
+
+        if(end != 0) System.out.println(end);
+        else System.out.println("-1");
     }
 }
 
