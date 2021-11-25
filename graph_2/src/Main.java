30bdd72be2d19e4eafa7720e71b0acce91c4ef93
xpexpe <84072084+xpexpe@users.noreply.github.com>
2021-11-26 00:59:25 +0900

    # COMPLETE PROBLEM

--- graph_2/src/Main.java
+++ graph_2/src/Main.java
@@ -1,10 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -15,7 +10,7 @@ public class Main {
         int M = Integer.parseInt(st.nextToken());
 
         ArrayList<LinkedList> graph = new ArrayList<>();
-        for(int i=0; i<N; i++)  graph.add(new LinkedList<Integer>());
+        for(int i=0; i<=N; i++)  graph.add(new LinkedList<Integer>());
 
         for(int i=0; i<M; i++) {
             st = new StringTokenizer(br.readLine());
@@ -30,9 +25,41 @@ public class Main {
         int p = Integer.parseInt(st.nextToken());
         int q = Integer.parseInt(st.nextToken());
 
+        boolean visited[] = new boolean[N+1];
+        for(int i=0; i<=N; i++) visited[i] = false;
 
-        Queue<Integer> queue = new
-        queue
+        int depth[] = new int[N+1];
+
+        Queue<Integer> queue = new LinkedList<>();
+
         queue.add(p);
+        visited[p] = true;
+        depth[p] = 0;
+        boolean find = false;
+
+
+        while(!queue.isEmpty()) {
+            int sel = queue.remove();
+            Iterator<Integer> it = graph.get(sel).iterator();
+            while(it.hasNext()) {
+                int num = it.next();
+                if(num == q)    find = true;
+                if(!visited[num]) {
+                    depth[num] = depth[sel] + 1;
+                    queue.add(num);
+                    visited[num] = true;
+                }
+            }
+        }
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        if(find) {
+            bw.write("아는 사람");
+            for(int i=1; i<depth[q]; i++) bw.write("의 아는 사람");
+        }
+        else bw.write("모르는 사람");
+
+        bw.flush();
     }
 }
