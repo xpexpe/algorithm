3873e0aba081d48f7229c13019cada91038e8981
xpexpe <84072084+xpexpe@users.noreply.github.com>
2021-11-25 22:28:08 +0900

    # CHECK 1

--- graph_2/src/Main.java
+++ graph_2/src/Main.java
@@ -1,5 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        ArrayList<LinkedList> graph = new ArrayList<>();
+        for(int i=0; i<N; i++)  graph.add(new LinkedList<Integer>());
+
+        for(int i=0; i<M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            graph.get(a).add(b);
+            graph.get(b).add(a);
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int p = Integer.parseInt(st.nextToken());
+        int q = Integer.parseInt(st.nextToken());
+
+
+        Queue<Integer> queue = new
+        queue
+        queue.add(p);
     }
 }
